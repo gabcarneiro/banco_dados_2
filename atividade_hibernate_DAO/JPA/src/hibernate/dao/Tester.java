21d003297d0b40a5a7a3e7b7688bf8c313a1da61
Gabriel Carneiro <gabcarneiro@outlook.com>
2017-10-13 17:55:07 -0300

    updates tester

--- atividade_hibernate_DAO/JPA/src/hibernate/dao/Tester.java
+++ atividade_hibernate_DAO/JPA/src/hibernate/dao/Tester.java
@@ -2,27 +2,27 @@ package hibernate.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
-public class Teste {
-	
+public class Tester {
+
 	public static void main(String[] args) {
-		
+
 		LivroDAO lDAO = new LivroDAO();
-		
+
 		Livro liv = new Livro();
 		liv.setAutor("Para");
 		liv.setTitulo("Ruby for Pros");
-		
+
 		Livro liv2 = new Livro();
 		liv2.setAutor("Ximbinha");
 		liv2.setTitulo("Java for Dummies");
-		
+
 		lDAO.save(liv);
 		lDAO.delete(new Livro(8));
-		
+
 		for (Livro l : lDAO.getAll()) {
 			System.out.println(l);
 		};
-	
+
 	}
 
 }
